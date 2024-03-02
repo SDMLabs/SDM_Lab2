5a1e82cb24fc7fa5a99fe954b1882d040c9b4d4c
aim4ik <rusak.yaroslav@lll.kpi.ua>
2024-03-02 15:36:35 +0200

    #5 added checks for errors
    - added check for coherent tags
    - added check for not closed tag symbols

--- HTMLParser.js
+++ HTMLParser.js
@@ -9,6 +9,9 @@ class HTMLParser {
             .replace(/_(\p{L}[\p{L}\p{M}\s_]*\p{L})_/gu, '<i>$1</i>')
             .replace(/`(\p{L}[\p{L}\p{M}\s_]*\p{L})`/gu, '<tt>$1</tt>')
 
+        HTMLParser._checkCoherence(markdownText);
+        HTMLParser._checkOpenedTags(markdownText);
+
         markdownText = markdownText.replace(/%%PRE_BLOCK%%/g, () => '<pre>' + preBlocks.shift() + '</pre>');
 
         const paragraphs = markdownText.split(/\n\s*\n/);
@@ -19,6 +22,29 @@ class HTMLParser {
         }
 
         return paragraphs.join('\n');
+    }
+
+    static _checkCoherence(markdownText) {
+        const tags = markdownText.match(/<\/?[^>]+>/g) || [];
+
+        for (let i = 0; i < tags.length; i += 2) {
+            if (tags[i] !== tags[i + 1].replace('/', '')) {
+                throw new Error("There are nested loops");
+            }
+        }
+    }
+
+    static _checkOpenedTags(markdownText) {
+        const words = markdownText.split(/\s+/);
+        const markdownSymbols = ['**', '_', '`'];
+        for (const word of words) {
+            console.log(word);
+            for (const symbol of markdownSymbols) {
+                if ((word.startsWith(symbol) || word.endsWith(symbol)) && word !== symbol) {
+                    throw new Error(`There is not finished markdown - ${word}`);
+                }
+            }
+        }
     }
 }
 
