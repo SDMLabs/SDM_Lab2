869b3cc2d2b023ddffed0079a8dbb6efde4b4c46
aim4ik <rusak.yaroslav@lll.kpi.ua>
2024-03-02 15:30:08 +0200

    Revert "#2 created HTMLParser"
    This reverts commit 6b522d993621ba7ff4712a676314d2a98aa48e5f.

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-const HTMLParser = require('./HTMLParser.js');
 
 function checkArguments(argv) {
     if (argv.length !== 3 && argv.length !== 5) {
@@ -12,29 +11,14 @@ function checkArguments(argv) {
 }
 
 function main() {
-    try {
-        const {argv} = process;
+    const {argv} = process;
 
-        checkArguments(argv);
+    checkArguments(argv);
 
-        const inputFile = argv[2];
-        const outputFile = argv[4];
+    const inputFile = argv[2];
+    const outputFile = argv[4];
 
-        const markdownText = fs.readFileSync(inputFile, 'utf-8');
-
-        const htmlText = HTMLParser.markdownToHTML(markdownText);
-
-        if (!outputFile) {
-            console.log(htmlText);
-        } else {
-            fs.writeFileSync(outputFile, htmlText);
-            console.log(`HTML розмітка була збережена в файлі: ${outputFile}`);
-        }
-
-    } catch (error) {
-        console.error('Помилка обробки файлу:', error);
-        process.exit(1);
-    }
+    const markdownText = fs.readFileSync(inputFile, 'utf-8');
 }
 
 main();
